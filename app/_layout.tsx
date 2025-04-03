4506a5432ac979cad575bcd63b23d6726d89f912
mhmd-abdelaziz <mabdelaziz@qurtobasolutions.com>
2025-04-04 00:46:17 +0200

    ADD: expo updates setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,30 @@
+import { useEffect } from "react";
 import { Slot } from "expo-router";
+import * as Updates from "expo-updates";
 import { client } from "@/apollo/client";
-import { AuthProvider } from "@/contexts/AuthContext";
 import { ApolloProvider } from "@apollo/client";
-import { ThemedView } from "@/components/ThemedView";
-import { ThemedText } from "@/components/ThemedText";
+import { AuthProvider } from "@/contexts/AuthContext";
+
+const checkForUpdates = async () => {
+  if (__DEV__) return;
+
+  try {
+    const update = await Updates.checkForUpdateAsync();
+
+    if (update.isAvailable) {
+      await Updates.fetchUpdateAsync();
+      await Updates.reloadAsync();
+    }
+  } catch (error) {
+    console.error("Error checking for updates:", error);
+  }
+};
 
 export default function Root() {
+  useEffect(() => {
+    checkForUpdates();
+  }, []);
+
   // Set up the auth context and render our layout inside of it.
   return (
     <ApolloProvider client={client}>
