0595276ea2f35b8991a2554d30edeef35c65f89f
mhmd-abdelaziz <mabdelaziz@qurtobasolutions.com>
2025-04-04 19:43:34 +0200

    REFACTOR: ThemedView component

--- components/shared/ThemedView.tsx
+++ components/shared/ThemedView.tsx
@@ -1,15 +1,34 @@
-import { View, type ViewProps } from "react-native";
-
+import {
+  View,
+  ViewStyle,
+  StyleProp,
+  useColorScheme,
+  type ViewProps,
+} from "react-native";
 import { useThemeColors } from "@/hooks";
 
-export type ThemedViewProps = ViewProps & {};
+export type ThemedViewProps = ViewProps & {
+  darkStyles?: StyleProp<ViewStyle>;
+  lightStyles?: StyleProp<ViewStyle>;
+};
 
-export default function ThemedView({ style, ...otherProps }: ThemedViewProps) {
+export default function ThemedView({
+  style,
+  darkStyles,
+  lightStyles,
+  ...otherProps
+}: ThemedViewProps) {
   const themeColors = useThemeColors();
+  const theme = useColorScheme() || "light";
 
   return (
     <View
-      style={[{ backgroundColor: themeColors.background }, style]}
+      style={[
+        { backgroundColor: themeColors.background },
+        style,
+        ...(theme === "dark" && darkStyles ? [darkStyles] : []),
+        ...(theme === "light" && lightStyles ? [lightStyles] : []),
+      ]}
       {...otherProps}
     />
   );
