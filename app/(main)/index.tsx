747de612253df2904a2d16f48078fc6e2f244760
mhmd-abdelaziz <mabdelaziz@qurtobasolutions.com>
2025-04-07 15:59:34 +0200

    ADD: logout

--- app/(main)/index.tsx
+++ app/(main)/index.tsx
@@ -5,13 +5,16 @@ import {
   FloatingButton,
 } from "@/components";
 import React from "react";
+import { useAuth } from "@/hooks";
 import { router } from "expo-router";
 import { Styles } from "@/constants";
 import { useQuery } from "@apollo/client";
 import { GET_CHATS } from "@/apollo/queries";
-import { View, FlatList } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { View, FlatList, TouchableOpacity } from "react-native";
 
 const Index = () => {
+  const { signOut } = useAuth();
   const { data, loading, refetch } = useQuery(GET_CHATS, {
     notifyOnNetworkStatusChange: true,
     variables: {
@@ -21,12 +24,24 @@ const Index = () => {
 
   const chats = data?.chats?.data || [];
 
+  const handleLogout = () => {
+    signOut();
+  };
+
   return (
     <>
-      <View style={Styles.appBar}>
+      <View
+        style={[
+          Styles.appBar,
+          { justifyContent: "space-between", alignItems: "center" },
+        ]}
+      >
         <ThemedText type="title" style={{ color: "white" }}>
           Mawared
         </ThemedText>
+        <TouchableOpacity onPress={handleLogout} style={{ padding: 10 }}>
+          <Ionicons name="log-out-outline" size={30} color="white" />
+        </TouchableOpacity>
       </View>
       <ThemedView style={Styles.screen}>
         <FlatList
